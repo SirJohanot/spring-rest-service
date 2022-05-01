135d47198cd7bbbdb539ed00f527b0961cd4a869
Johanot <67147458+SirJohanot@users.noreply.github.com>
2022-05-01 13:33:21 +0300

    added integration tests

--- src/test/java/com/springfeatures/springrestservice/SpringRestServiceApplicationTests.java
+++ src/test/java/com/springfeatures/springrestservice/SpringRestServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringRestServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
